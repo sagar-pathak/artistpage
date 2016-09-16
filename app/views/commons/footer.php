f2c487de63ddf6aadf7039a4dd74f1d554a01984
Sagar Pathak <surfer.sagar@gmail.com>
2016-09-16 14:09:42 +0545

    footer added

--- app/views/commons/footer.php
+++ app/views/commons/footer.php
@@ -1,3 +1,33 @@
+   <!-- footer text: starts -->
+	<div class="splash">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <!-- <div><img class="logo" src="assets/img/logo.png"></div> -->
+            <h2>SUBSCRIBE TO OUR
+            <br/>
+	            <span class="fa fa-youtube"></span>
+	            <span>YOUTUBE CHANNEL</span>
+            </h2>
+				<script src="https://apis.google.com/js/platform.js"></script>
+				<div class="g-ytsubscribe" data-channel="GoogleDevelopers" data-layout="default" data-count="default"></div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div class="container-fluid footer">
+	    <div class="container">
+          	<div class="copyright pull-left">
+	          	Copyrights © 2016 All Rights Reserved by Artist<br/>
+				Terms of Use / Privacy Policy
+			</div>
+			<div class="copyright pull-right">
+	          	Developed and maintained by <a href="#">Sagar</a>
+			</div>
+	    </div>
+	 </div>
+   <!-- footer text: ends -->
     <script src="<?php echo JS_DIR; ?>jquery-1.10.2.min.js"></script>
     <script src="<?php echo JS_DIR; ?>jquery.fitvids.js"></script>
     <script src="<?php echo JS_DIR; ?>bootstrap/bootstrap.min.js"></script>
